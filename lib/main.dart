1f896f6630c00714c8f98ed53955bd6b5cda5e1a
Alexandre ROUSSEL <alexandre.roussel@insa-lyon.fr>
2020-11-14 21:36:30 +0100

    Add alerting

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong/latlong.dart';
 
+import 'src/alert.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(SecurowApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SecurowApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SecuRow',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.deepPurple,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'SecuRow le S'),
@@ -36,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,16 +34,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: FlutterMap(
@@ -96,7 +68,13 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AlertPage()));
+        },
+        child: Icon(Icons.notifications_active),
+      ),
     );
   }
 }
---
+++ lib/src/alert.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:volume_control/volume_control.dart';
+import 'package:vibration/vibration.dart';
+
+class AlertPage extends StatefulWidget {
+  // AlertPage({Key key, this.title}) : super(key: key);
+
+  // final String title;
+  final player = AudioPlayer();
+
+  @override
+  _AlertPageState createState() => _AlertPageState();
+}
+
+class _AlertPageState extends State<AlertPage> {
+  _backToMap() {
+    widget.player.stop();
+    Vibration.cancel();
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Vibration.vibrate(pattern: [250, 1000], repeat: 1);
+    // VolumeControl.setVolume(1);
+    widget.player.setAsset('assets/sounds/alarm.mp3');
+    widget.player.setLoopMode(LoopMode.one);
+    widget.player.play();
+
+    return WillPopScope(
+      onWillPop: () => _backToMap(),
+      child: Scaffold(
+        backgroundColor: Colors.red[800],
+        body: GestureDetector(
+          onTap: () => _backToMap(),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.warning_amber_rounded,
+                  color: Colors.white,
+                  size: 200,
+                ),
+                Text(
+                  'Un bateau a besoin d\'aide !',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 80),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
